26b89d73bc5eea2f582b4c3b680bd1fd9a48b7c4
NovyFur <novyhusky@gmail.com>
2025-09-22 00:30:54 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   resolve: {
     alias: {
